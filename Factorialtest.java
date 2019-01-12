0106646a3f10e71216b3188db045fabd04e3cbae
RGumma <46632489+RGumma@users.noreply.github.com>
2019-01-12 13:35:45 -0600

    Add files via upload

---
+++ Factorialtest.java
@@ -0,0 +1,14 @@
+
+public class Factorialtest {
+
+	public static void main(String[] args) {
+		
+		Factorial f1 =new Factorial();
+		f1.factorial(6);
+		
+		System.out.println(f1.factorial(6));
+		
+		
+	}
+
+}
